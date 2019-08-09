2a0351e0b7932de0ec408b349639ec6b9e4763be
Larry Wright <psyconerd@gmail.com>
2019-08-09 07:02:37 -0400

    saving for work

--- changes/Form1.Designer.cs
+++ changes/Form1.Designer.cs
@@ -48,120 +48,135 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(242, 21);
+            this.label1.Location = new System.Drawing.Point(363, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(106, 21);
+            this.label2.Location = new System.Drawing.Point(159, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(88, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "ulong/short";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(106, 53);
+            this.label3.Location = new System.Drawing.Point(159, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(90, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "64-16bit val";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(242, 53);
+            this.label4.Location = new System.Drawing.Point(363, 82);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(51, 20);
             this.label4.TabIndex = 2;
             this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(106, 88);
+            this.label5.Location = new System.Drawing.Point(159, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.Size = new System.Drawing.Size(96, 20);
             this.label5.TabIndex = 5;
             this.label5.Text = "decimal max";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(242, 88);
+            this.label6.Location = new System.Drawing.Point(363, 135);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(51, 20);
             this.label6.TabIndex = 4;
             this.label6.Text = "label6";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(106, 121);
+            this.label7.Location = new System.Drawing.Point(159, 186);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.Size = new System.Drawing.Size(72, 20);
             this.label7.TabIndex = 7;
             this.label7.Text = "long max";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(242, 121);
+            this.label8.Location = new System.Drawing.Point(363, 186);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(51, 20);
             this.label8.TabIndex = 6;
             this.label8.Text = "label8";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(106, 155);
+            this.label9.Location = new System.Drawing.Point(159, 238);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.Size = new System.Drawing.Size(90, 20);
             this.label9.TabIndex = 9;
             this.label9.Text = "double max";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(242, 155);
+            this.label10.Location = new System.Drawing.Point(363, 238);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.Size = new System.Drawing.Size(60, 20);
             this.label10.TabIndex = 8;
             this.label10.Text = "label10";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(106, 212);
+            this.label11.Location = new System.Drawing.Point(159, 326);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(54, 13);
+            this.label11.Size = new System.Drawing.Size(79, 20);
             this.label11.TabIndex = 18;
             this.label11.Text = "MaxValue";
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox2.Location = new System.Drawing.Point(167, 209);
+            this.textBox2.Location = new System.Drawing.Point(250, 322);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(307, 20);
+            this.textBox2.Size = new System.Drawing.Size(458, 26);
             this.textBox2.TabIndex = 17;
+            this.textBox2.Text = "0.00567";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(480, 207);
+            this.button3.Location = new System.Drawing.Point(720, 318);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(78, 23);
+            this.button3.Size = new System.Drawing.Size(117, 35);
             this.button3.TabIndex = 16;
             this.button3.Text = "Generate";
             this.button3.UseVisualStyleBackColor = true;
@@ -169,44 +184,49 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(615, 212);
+            this.label12.Location = new System.Drawing.Point(922, 326);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.Size = new System.Drawing.Size(60, 20);
             this.label12.TabIndex = 19;
             this.label12.Text = "label12";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(615, 241);
+            this.label13.Location = new System.Drawing.Point(922, 371);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.Size = new System.Drawing.Size(60, 20);
             this.label13.TabIndex = 23;
             this.label13.Text = "label13";
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(106, 241);
+            this.label14.Location = new System.Drawing.Point(159, 371);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(55, 13);
+            this.label14.Size = new System.Drawing.Size(80, 20);
             this.label14.TabIndex = 22;
             this.label14.Text = "Log2 Test";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox1.Location = new System.Drawing.Point(167, 238);
+            this.textBox1.Location = new System.Drawing.Point(250, 366);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 20);
+            this.textBox1.Size = new System.Drawing.Size(458, 26);
             this.textBox1.TabIndex = 21;
-            this.textBox1.Text = "699999999999999999999999999";
+            this.textBox1.Text = "0.00567";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(480, 236);
+            this.button1.Location = new System.Drawing.Point(720, 363);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.Size = new System.Drawing.Size(117, 35);
             this.button1.TabIndex = 20;
             this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
@@ -214,25 +234,38 @@
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox3.Location = new System.Drawing.Point(167, 265);
+            this.textBox3.Location = new System.Drawing.Point(250, 408);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(307, 20);
+            this.textBox3.Size = new System.Drawing.Size(458, 26);
             this.textBox3.TabIndex = 24;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(480, 265);
+            this.button2.Location = new System.Drawing.Point(720, 408);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.Size = new System.Drawing.Size(117, 35);
             this.button2.TabIndex = 25;
             this.button2.Text = "Generate";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(922, 415);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(60, 20);
+            this.label15.TabIndex = 26;
+            this.label15.Text = "label15";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label15);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label13);
@@ -253,6 +286,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -283,6 +317,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label15;
     }
 }
 
--- changes/Form1.cs
+++ changes/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace changes
 {
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll", SetLastError = false)]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
         public Form1()
         {
             this.Shown += Form1_Shown;
@@ -32,13 +36,24 @@ namespace changes
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            if (!decimal.TryParse(textBox1.Text, out decimal r)) { return; }
-            double d = (double)r;
-            label13.Text = ((int)(Math.Truncate(Math.Log(d, 2))) + 1).ToString();
-            decimal r2d2 = (decimal)d;
-            textBox3.Text = r2d2.ToString();
-        }
+            if (!decimal.TryParse(textBox1.Text, out decimal decValue)) { return; }
+            double dubValue = (double)decValue;
+            label13.Text = ((int)(Math.Truncate(Math.Log(dubValue, 2))) + 1).ToString();
+            decimal redubValue = (decimal)dubValue;
+            textBox3.Text = redubValue.ToString();
 
+            int digits = 1;
+            ulong multilpier = (ulong)Math.Pow(10, GetScale(decValue));
+            label12.Text = multilpier.ToString();
+            decValue = decValue * multilpier;
+            textBox1.Text = decValue.ToString();
+            while (CreateMaxDecimalFromBitCount(digits) < decValue) { digits++; }
+            label15.Text = digits.ToString();
+        }
+        private byte GetScale(decimal dVal)
+        {
+            return BitConverter.GetBytes(Decimal.GetBits(dVal)[3])[2];
+        }
         private void Button3_Click(object sender, EventArgs e)
         {
             // decimal places
@@ -49,11 +64,51 @@ namespace changes
             int mid = parts[1];
             int hi = parts[2];
             bool sign = (parts[3] & 0x80000000) != 0;
-            byte scale = (byte)((parts[3] >> 16) & 0x7F);
+            //byte scale = (byte)((parts[3] >> 16) & 0x7F);
             //scale = BitConverter.GetBytes(decimal.GetBits(dVal)[3])[2];
-            scale = BitConverter.GetBytes(parts[3])[2];
+            byte scale = BitConverter.GetBytes(parts[3])[2];
             decimal d = new decimal(lo: lo, mid: mid, hi: hi, isNegative: sign, scale: scale);
             label12.Text = scale.ToString();
+        }
+        private decimal CreateMaxDecimalFromBitCount(int bits)
+        {
+            if (bits > 95) { throw new ArgumentException($"The number of bits, {bits} can not be converted to a decimal value."); }
+            int lo = 0;
+            int mid = 0;
+            int hi = 0;
+            bool sign = false;
+            byte scale = 0;
+            if (bits > 64)
+            { // 3 ints 2 ints full, 1 partial
+                lo = CreateMaxIntFromBitCount(32);
+                mid = CreateMaxIntFromBitCount(32);
+                hi = CreateMaxIntFromBitCount(bits - 64);
+            }
+            else if (bits > 32)
+            {// 2 ints 1 ints full, 1 partial
+                lo = CreateMaxIntFromBitCount(32);
+                mid = CreateMaxIntFromBitCount(bits - 32);
+                hi = CreateMaxIntFromBitCount(0);
+            }
+            else if (bits > 0)
+            { // 1 int, 1 partial
+                lo = CreateMaxIntFromBitCount(bits);
+                mid = CreateMaxIntFromBitCount(0);
+                hi = CreateMaxIntFromBitCount(0);
+            }
+            else { throw new ArgumentException($"The number of bits, {bits} can not be converted to a decimal value."); }
+            return new decimal(lo: lo, mid: mid, hi: hi, isNegative: sign, scale: scale);
+        }
+        private int CreateMaxIntFromBitCount(int bits)
+        {
+            if (bits < 1) { return 0; }
+            int returnValue = 1;
+            if (bits > 1) { for (int i = 0; i < bits - 1; i++) { returnValue = returnValue << 1; returnValue = returnValue | 1; } }
+            return returnValue;
+        }
+
+        private void MinBits()
+        {
 
         }
 
